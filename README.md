75c3d04ac8d131c44d8d03ff507a8d50094a0e1f
Obiageli Ogbutor <obygirl81@gmail.com>
2019-09-01 23:28:35 -0600

    uploading completed game files

--- README.md
+++ README.md
@@ -1,1 +1,14 @@
 # Constructor-Word-Guess-command-line-game
+Advanced JavaScript: Constructor Word Guess Game
+
+This is a Word Guess command-line application game developed using constructor functions. In this game, user will guess the correct name of the longest river randomly selected by the computer on  the screen. user will have 10 chances to guess the correct name. When the user guesses a correct name, a message is displayed to user that you win. If user fails to guess the correct longest river name, a message is displayed to user that you lose. Do you know the longest rivers in the United States of America? Test your knowledge and have fun!
+
+This game receives user input using the inquirer or prompt npm packages.
+
+Letter.js: Contains a constructor, Letter. This constructor displays an underlying character or a blank placeholder (such as an underscore), depending on whether or not the user has guessed the letter. 
+
+Word.js: Contains a constructor, Word that depends on the Letter constructor. This is used to create an object representing the current word the user is attempting to guess. 
+
+index.js: This is the file containing the logic for the course of the game, which depends on Word.js.  It randomly selects a word and uses the Word constructor to store it, prompts the user for each guess, and keeps track of the user's remaining guesses
+
+See the attached screenshots and here is the link to check out the game: 
---
+++ index.js
@@ -0,0 +1,151 @@
+
+var Word = require("./word.js");
+var inquirer = require("inquirer");
+
+var letterArray = "abcdefghijklmnopqrstuvwxyz";
+
+// words list
+var longestRiver = ["missouri", "mississippi", "yukon", "rio grande", "colorado", "arkansas", "columbia", "cameroon", "red", "snake", "ohio", "colorado river of texas", "tennessee", "canadian", "brazos", "green", "pecos", "white river arkansas", "james", "kuskokwim", "cimarron", "cumberland", "yellow stone", "north platte", "milk", "ouachita", "saint lawrence", "gila", "sheyenne", "tanana", "smoky hill", "niobrara", "little missouri", "sabine", "red river of the north", "des moines", "white river missouri", "trinity", "wabash"];
+
+// Random name from longestRiver array
+var randomName = Math.floor(Math.random() * longestRiver.length);
+var randomWord = longestRiver[randomName];
+
+// Passing random word via Word constructor
+computerWord = new Word(randomWord);
+
+var requireNewWord = false;
+
+// Array to hold guessed letters
+var incorrectLetters = [];
+var correctLetters = [];
+
+// Guesses left
+var guessesLeft = 10;
+
+function knowledge() {
+
+    // Creates new word for the Word constructor when true
+    if (requireNewWord) {
+        // Selects the random longestRiver array
+        var randomName = Math.floor(Math.random() * longestRiver.length);
+        var randomWord = longestRiver[randomName];
+
+        // Passing random word through the Word constructor
+        computerWord = new Word(randomWord);
+
+        
+        requireNewWord = false;
+    }
+
+
+    // Checks when a letter guessed is correct
+    var wordComplete = [];
+    computerWord.objArray.forEach(completeCheck);
+
+    // the remaining letters to guess
+    if (wordComplete.includes(false)) {
+        inquirer
+            .prompt([
+                {
+                    type: "input",
+                    message: "Guess any letter between A-Z!",
+                    name: "userinput"
+                }
+            ])
+            .then(function (input) {
+
+               
+                if (!letterArray.includes(input.userinput) || input.userinput.length > 1) {
+                    console.log("\nGuess again!\n");
+                    knowledge();
+                } else {
+
+                   
+                    if (incorrectLetters.includes(input.userinput) || correctLetters.includes(input.userinput) || input.userinput === "") {
+                        console.log("\nAlready Guessed or Nothing Guessed\n");
+                        knowledge();
+                    } else {
+
+                        // Checks when user guess is correct
+                        var wordCheckArray = [];
+
+                        
+                        computerWord.userGuess(input.userinput);
+
+                        computerWord.objArray.forEach(wordCheck);
+                        if (wordCheckArray.join('') === wordComplete.join('')) {
+                            console.log("\nIncorrect\n");
+                           
+                            incorrectLetters.push(input.userinput);
+                            guessesLeft--;
+                        } else {
+                            console.log("\nCorrect!\n");
+                           
+                            correctLetters.push(input.userinput);
+                        }
+
+                        
+                        computerWord.log();
+
+                        // show remianing guesses left
+                        console.log("Guesses Left: " + guessesLeft + "\n");
+ 
+                        // show the letters already guessed
+                        console.log("Letters Guessed: " + incorrectLetters.join(" ") + "\n");
+
+                        // User number of guess left
+                        if (guessesLeft > 0) {
+                            
+                            knowledge();
+                        } else {
+                            console.log("You lose!\n");
+
+                            restartGame();
+                        }
+
+
+                        
+                        function wordCheck(key) {
+                            wordCheckArray.push(key.guessed);
+                        }
+                    }
+                }
+            })
+    } else {
+        console.log("You win!\n");
+
+        restartGame();
+    }
+
+   
+    function completeCheck(key) {
+        wordComplete.push(key.guessed);
+    }
+
+}
+
+function restartGame() {
+    inquirer
+        .prompt([
+            {
+                type: "list",
+                message: "What would you like to do:",
+                choices: ["Replay game", "Exit"],
+                name: "restart"
+            }
+        ])
+        .then(function (input) {
+            if (input.restart === "Replay game") {
+                requireNewWord = true;
+                incorrectLetters = [];
+                correctLetters = [];
+                guessesLeft = 10;
+                knowledge();
+            } else {
+                return
+            }
+        })
+}
+
+knowledge();
